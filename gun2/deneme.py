be5c76b55839ef47d2864573ee2f001b64455261
ThezMArda <mehmerardaa@gmail.com>
2025-12-07 11:57:43 +0300

    2. gun başlangıcı

--- gun2/deneme.py
+++ gun2/deneme.py
@@ -1,1 +1,9 @@
-print("hello")
+from typing import Optional
+def add(a:int,b:int,c: Optional[int]=None)-> int:
+    if c is None:
+        return a+b
+    else:
+        return a+b+c
+
+print(add(1,2))
+print(add(1,2,3))
